8f133bb6ab75c62bc3361f299539b38527f79be7
alilco <123931406+alilco@users.noreply.github.com>
2024-02-23 16:28:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-from user_agent import generate_user_agent
 from flask import *
 
 app = Flask(__name__)
@@ -8,80 +7,44 @@ app = Flask(__name__)
 def info(email):
     email1 = email
     try:
-        url = 'https://www.instagram.com/api/v1/web/accounts/check_email/'
+        url = 'https://www.instagram.com/api/v1/web/accounts/login/ajax/?hl=ar'
         headers = {
-	
-		 'Host': 'www.instagram.com',
-	
-		 'content-length': '22',
-	
-		 'sec-ch-ua': '"Android WebView";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
-	
-		 'x-ig-www-claim': '0',
-	
-		 'sec-ch-ua-platform-version': '"11.0.0"',
-	
-		 'x-requested-with': 'XMLHttpRequest',
-	
-		 'dpr': '2',
-	
-		 'sec-ch-ua-full-version-list': '"Android WebView";v="119.0.6045.163", "Chromium";v="119.0.6045.163", "Not?A_Brand";v="24.0.0.0"',
-	
-		 'x-csrftoken': 'VePQfPfUYHlwXGsHe74dBAYcUJ4dwNR6',
-	
-		 'sec-ch-ua-model': '"RMX3231"',
-	
-		 'sec-ch-ua-platform': '"Android"',
-	
-		 'x-ig-app-id': '1217981644879628',
-	
-		 'sec-ch-prefers-color-scheme': 'light',
-	
-		 'sec-ch-ua-mobile': '?1',
-	
-		 'x-instagram-ajax': '1010142781',
-	
-		 'user-agent': generate_user_agent(),
-	
-		 'viewport-width': '360',
-	
-		 'content-type': 'application/x-www-form-urlencoded',
-	
-		 'accept': '*/*',
-	
-		 'x-asbd-id': '129477',
-	
-		 'origin': 'https://www.instagram.com',
-	
-		 'sec-fetch-site': 'same-origin',
-	
-		 'sec-fetch-mode': 'cors',
-	
-		 'sec-fetch-dest': 'empty',
-	
-		 'referer': 'https://www.instagram.com/accounts/signup/email/',
-	
-		 'accept-encoding': 'gzip, deflate, br',
-	
-		 'accept-language': 'ar-IQ,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-	
-		 'cookie': 'csrftoken=VePQfPfUYHlwXGsHe74dBAYcUJ4dwNR6',
-	
-		 'cookie': 'mid=ZWlr6AABAAHM2vzmuOS-uwLYhgXX',
-	
-		 'cookie': 'ig_nrcb=1',
-	
-		 'cookie': 'datr=5GtpZXKDqnzbfcIZg0FLkRqv',
-	
-		 'cookie': 'ig_did=B2B00EA7-37E3-45D1-B806-DA1FFFB7D82D',
-	
-	}
-        data = {
-            'email':f'{email1}@gmail.com'
-        }
-        response = requests.post(url, headers=headers, data=data).text
-        if '"email_is_taken"' in response:
-            return '{"email_is_taken",}'
+    'Host': 'www.instagram.com',
+    'content-length': '322',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'sec-ch-ua': '"Not A(Brand";v="99", "Android WebView";v="121", "Chromium";v="121"',
+    'x-ig-www-claim': '0',
+    'sec-ch-ua-platform-version': '"10.0.0"',
+    'x-requested-with': 'XMLHttpRequest',
+    'x-web-device-id': '0A2B0FB8-243D-4F72-9781-1E456CB34DC8',
+    'dpr': '1.75',
+    'sec-ch-ua-full-version-list': '"Not A(Brand";v="99.0.0.0", "Android WebView";v="121.0.6167.178", "Chromium";v="121.0.6167.178"',
+    'x-csrftoken': 'PWlSM95SKa80_wY819MwNH',
+    'sec-ch-ua-model': '"Lenovo K12"',
+    'sec-ch-ua-platform': '"Android"',
+    'x-ig-app-id': '1217981644879628',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua-mobile': '?1',
+    'x-instagram-ajax': '1011606959',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; Lenovo K12 Build/QOGS30.569-83-18; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/121.0.6167.178 Mobile Safari/537.36',
+    'viewport-width': '412',
+    'content-type': 'application/x-www-form-urlencoded',
+    'accept': '*/*',
+    'x-asbd-id': '129477',
+    'origin': 'https://www.instagram.com',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://www.instagram.com/accounts/login/?hl=ar',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ar-IQ,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cookie': 'csrftoken=PWlSM95SKa80_wY819MwNH;ps_l=0;ps_n=0;ig_did=0A2B0FB8-243D-4F72-9781-1E456CB34DC8;ig_nrcb=1;dpr=1.75;_js_ig_did=0A2B0FB8-243D-4F72-9781-1E456CB34DC8;_js_datr=doHYZdH9fk1pr9niSosv4FFl;mid=ZdiBdgABAAECWGqUQvAJf2vOsjxC',
+}
+        data = (f'enc_password=%23PWD_INSTAGRAM_BROWSER%3A10%3A1708687786%3AAWdQAA5dXzWQAlVLYNbJZrI8%2FJcAYp1RZbsMXgS9GbYb7RBTnSDKsP12sToWYUeDQn56NuWmG2N%2BOyDdE51YdokpSN2tJl8mK80MmSWdDbdIMXW1Hv%2FB3XzvejomEPng%2B%2Buof2rARLuB85W2yiY%3D&optIntoOneTap=false&queryParams=%7B%22hl%22%3A%22ar%22%7D&trustedDeviceRecords=%7B%7D&username={email}')
+        req = requests.post(url, headers=headers, data=data).text
+        if '"user":true' in req:
+            return '{"user":true}'
         else:
             return '{"False":"email"}'
     except IndexError:
