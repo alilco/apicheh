da3c4011185f8379eb98ad433a9620678aa6b897
F O [X] <123931406+alilco@users.noreply.github.com>
2024-07-28 13:19:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/api/info=<email>')
 def check(email):
-    try:
-        headers = {
+    em = email
+    headers = {
                 'accept': '*/*',
                 'accept-language': 'en-US,en;q=0.9',
                 'content-type': 'application/x-www-form-urlencoded',
@@ -14,19 +14,16 @@ def check(email):
                 'referer': 'https://www.instagram.com/?lang=en-US&hl=en-gb',
                 'user-agent': 'Mozilla/5.0 (Linux; Android 13; ANY-LX2 Build/HONORANY-L22CQ; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/125.0.6422.147 Mobile Safari/537.36 InstagramLite 1.0.0.0.145 Android (33/13; 480dpi; 1080x2298; HONOR; ANY-LX2; HNANY-Q1; qcom; ar_IQ_#u-nu-latn; 115357035)',
                 'x-csrftoken': '9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
-            }
+    }
 
-        data = {
-            'username': email,
-        }
+    data = {
+            'username': em,
+    }
 
-        response = requests.post('https://www.instagram.com/api/v1/web/accounts/login/ajax/', headers=headers, data=data).text
-        if 'showAccountRecoveryModal' in response:
-            return {"FOX":"@UUYFU" ,"email":"good"}
-        else:
-            return {"FOX":"@UUYFU" ,"email":"bad"}
-
-    except:return {"FOX":"@UUYFU" ,"email":"bad"}
-
+    response = requests.post('https://www.instagram.com/api/v1/web/accounts/login/ajax/', headers=headers, data=data).text
+    if 'showAccountRecoveryModal' in response:
+        return {"FOX":"@UUYFU" ,"email":"good"}
+    else:
+        return {"FOX":"@UUYFU" ,"email":"bad"}
 if __name__ == "__main__":
 	app.run()
